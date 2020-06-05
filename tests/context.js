3e2a8332c6e6de940f2d6957907c9e5cc78d5627
Sergio Garcia Murillo <sergio.garcia.murillo@gmail.com>
2020-06-05 02:56:34 +0200

    Add receiver key timeout after ratcheting or setting a new key (close #4)

--- tests/context.js
+++ tests/context.js
@@ -1,6 +1,11 @@
 import {Context}	from "../lib/Context.js";
 import {Utils}		from "../lib/Utils.js";
 
+function sleep(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
 tape.test("context", async function(suite){
 	
 	await suite.test("100 frames audio",async function(test){
@@ -297,5 +302,157 @@ tape.test("context", async function(suite){
 			}
 		}
 		
+	});
+	
+	await suite.test("Allow duplicate frames",async function(test){
+		
+		test.plan(2);
+		
+		const shared = Utils.fromHex("1234567890123456789012345678901212345678901234567890123456789012");
+		const keyPair = await window.crypto.subtle.generateKey (
+			{
+				name: "ECDSA",
+				namedCurve: "P-521"
+			},
+			false,
+			["sign", "verify"]
+		);
+		const sender = new Context(0);
+		const receiver = new Context(1);
+		
+		await sender.setSenderEncryptionKey(shared);
+		await sender.setSenderSigningKey (keyPair.privateKey);
+		
+		receiver.addReceiver(0);
+		await receiver.setReceiverEncryptionKey(0,shared);
+		await receiver.setReceiverVerifyKey(0,keyPair.publicKey);
+		
+		const frame = Utils.fromHex("cacadebaca");
+		const encrypted = await sender.encrypt("audio",0,frame);
+		const decrypted1 = await receiver.decrypt("audio",0,encrypted);
+		const decrypted2 = await receiver.decrypt("audio",0,encrypted);
+		test.same(Utils.toHex(frame),Utils.toHex (decrypted1));
+		test.same(Utils.toHex(frame),Utils.toHex (decrypted2));
+		
+	});
+	
+	await suite.test("Update key",async function(test){
+		
+		test.plan(9);
+		
+		const shared = Utils.fromHex("1234567890123456789012345678901212345678901234567890123456789010");
+		
+		const sender = new Context(0);
+		const receiver = new Context(1);
+		
+		await sender.setSenderEncryptionKey(shared);
+		receiver.addReceiver(0);
+		await receiver.setReceiverEncryptionKey(0,shared);
+		
+		for (let i=1;i<10;i++)
+		{
+			const frame = Utils.fromHex("cacadebaca"+i);
+			const encrypted = await sender.encrypt("audio",0,frame);
+			const decrypted = await receiver.decrypt("audio",0,encrypted);
+			test.same(Utils.toHex(frame),Utils.toHex (decrypted));
+			
+			//Update key
+			const updated = Utils.fromHex("123456789012345678901234567890121234567890123456789012345678901"+i);
+			await sender.setSenderEncryptionKey(updated);
+			await receiver.setReceiverEncryptionKey(0,updated);
+		}
+		
+	});
+	
+	
+	await suite.test("key timeout on new key setup",async function(test){
+		
+		test.plan(3);
+		
+		const shared  = Utils.fromHex("1234567890123456789012345678901212345678901234567890123456789010");
+		const updated = Utils.fromHex("1234567890123456789012345678901212345678901234567890123456789011");
+		
+		const sender = new Context(0);
+		const receiver = new Context(1);
+		
+		await sender.setSenderEncryptionKey(shared);
+		receiver.addReceiver(0);
+		//Set both keys on receiver
+		await receiver.setReceiverEncryptionKey(0,shared);
+		await receiver.setReceiverEncryptionKey(0,updated);
+		const frame1 = Utils.fromHex("cacadebaca1");
+		const frame2 = Utils.fromHex("cacadebaca2");
+		
+		//Encrypt first one with shared
+		const encrypted1 = await sender.encrypt("audio",0,frame1);
+		
+		//Encrypte second one with updated
+		await sender.setSenderEncryptionKey(updated);
+		const encrypted2 = await sender.encrypt("audio",0,frame2);
+		
+		//Both should work now
+		const decrypted1 = await receiver.decrypt("audio",0,encrypted1);
+		const decrypted2 = await receiver.decrypt("audio",0,encrypted2);
+		test.same(Utils.toHex(frame1),Utils.toHex (decrypted1));
+		test.same(Utils.toHex(frame2),Utils.toHex (decrypted2));
+			
+		//Wait for timeout
+		await sleep(1000);
+		
+		try 
+		{
+			//Decrypt frame with expeired key
+			const decrypted1 = await receiver.decrypt("audio",0,encrypted1);
+			//SHould have failed
+			test.fail();
+		} catch (e) {
+			//Should fail 
+			test.ok(e);
+		}
+	});
+	
+	await suite.test("key timeout on ratchet",async function(test){
+		
+		test.plan(3);
+		
+		const shared  = Utils.fromHex("1234567890123456789012345678901212345678901234567890123456789010");
+		
+		const sender = new Context(0);
+		const receiver = new Context(1);
+		
+		await sender.setSenderEncryptionKey(shared);
+		receiver.addReceiver(0);
+		await receiver.setReceiverEncryptionKey(0,shared);
+		
+		
+		const frame1 = Utils.fromHex("cacadebaca1");
+		const frame2 = Utils.fromHex("cacadebaca2");
+		
+		//Encrypt first one with shared
+		const encrypted1 = await sender.encrypt("audio",0,frame1);
+		
+		//Encrypte second one with tatchet key
+		await sender.ratchetSenderEncryptionKey();
+		const encrypted2 = await sender.encrypt("audio",0,frame2);
+		
+		//Both should work now
+		const decrypted1 = await receiver.decrypt("audio",0,encrypted1);
+		const decrypted2 = await receiver.decrypt("audio",0,encrypted2);
+		test.same(Utils.toHex(frame1),Utils.toHex (decrypted1));
+		test.same(Utils.toHex(frame2),Utils.toHex (decrypted2));
+			
+		//Wait for timeout
+		await sleep(1000);
+		
+		try 
+		{
+			//Decrypt frame with expeired key
+			const decrypted1 = await receiver.decrypt("audio",0,encrypted1);
+			//SHould have failed
+			test.fail();
+		} catch (e) {
+			//Should fail 
+			test.ok(e);
+		}
 	});
 });
